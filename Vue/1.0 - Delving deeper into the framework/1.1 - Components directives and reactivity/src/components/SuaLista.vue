92f428592fbac178c44bef7027aa9e463100b481
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2025-01-14 18:29:51 -0300

    1.1 Vue

---
+++ Vue/1.0 - Delving deeper into the framework/1.1 - Components directives and reactivity/src/components/SuaLista.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default {
+    props: {
+        ingredientes: { type: Array, required: true }
+    }
+}
+</script>
+
+<template>
+    <section>
+        <span class="subtitulo-lg sua-lista-texto">
+        </span>
+
+        <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+            <li v-for="(ingrediente, index) in ingredientes" :key="index">
+                <Tag :texto="ingrediente" ativa />
+            </li>
+        </ul>
+
+        <p v-else class="paragrafo lista-vazia">
+            <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de pesquisa">
+            Sua lista está vazia, selecione itens para iniciar.
+        </p>
+    </section>
+</template>
